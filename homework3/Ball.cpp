0af79e7f31d6c7ad4945194c290bb293d62e8881
kate301216cat <104170157+kate301216cat@users.noreply.github.com>
2022-12-10 16:12:23 +0300

    Add homework 3

---
+++ homework3/Ball.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+struct Point {
+    float x, y;
+};
+struct Velocity {
+    float vx, vy;
+};
+struct Stats {
+    Point point;
+    Velocity velocity;
+    double time;
+};
+
+class Ball {
+    Point point;
+    Velocity velocity;
+    double time;
+    Stats stats;
+    const float g = 9.81;
+    
+public:
+    void push(double alpha, double v) {
+        velocity.vx += v * cos(alpha);
+        velocity.vy += v * sin(alpha);
+    }
+    void fly(double t) {
+        double x_new, y_new;
+        x_new = point.x + velocity.vx * t;
+        y_new = point.y + velocity.vy * t - 0.5*g*t*t;
+        point.x = x_new;
+        point.y = y_new;
+    }
+    Point getPosition() {
+        return point;
+    }
+    Velocity getVelocity() {
+        return velocity;
+    }
+    Stats getStats() {
+        stats.point = point;
+        stats.velocity = velocity;
+        stats.time = time;
+        return stats;
+    }
+   
+};
+
+int main()
+{
+
+
+}
+
---
+++ homework3/PhysClass.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Energy
+{
+private:
+    float energy;
+public:
+    static long SGS;
+    static float eV;
+    void setEnergy(int quantity, int type)
+    {
+        switch (type) {
+        case 1:
+            energy = quantity;
+            break;
+        case 2:
+            energy = quantity / SGS;
+            break;
+        case 3:
+            energy = quantity / eV;
+            break;
+        }
+    }
+    float getEnergy(int type) {
+        switch (type) {
+        case 1:
+            return energy;
+            break;
+        case 2:
+            return energy * SGS;
+            break;
+        case 3:
+            return energy * eV;
+            break;
+        }
+    }
+    
+};
+
+long Energy::SGS = pow(10, 7);
+float Energy::eV = 1.6;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int typeIn, typeOut;
+    long quantity;
+    cout << "Выберите, в каких единицах вы введёте энергию (1 - Дж, 2 - эрг, 3 - эВ*10^19): ";
+    cin >> typeIn;
+    cout << "Введите значение энергии: ";
+    cin >> quantity;
+    Energy energy;
+    energy.setEnergy(quantity, typeIn);
+    cout << "Выберите, в каких единицах вы хотите получить энергию (1 - Дж, 2 - эрг, 3 - эВ*10^19): ";
+    cin >> typeOut;
+    cout << "Ваш ответ: " << energy.getEnergy(typeOut);
+
+
+}
+
